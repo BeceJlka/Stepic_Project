3bf660082746a8fa2c56e194a4c9c14ef5569d9e
BeceJlka <59609028+BeceJlka@users.noreply.github.com>
2020-02-23 19:09:36 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from data import *
 import random
+
 app = Flask(__name__)
 random_tour = []
 search_tours = []
@@ -10,9 +11,10 @@ for i in range(6):
     a = random.randint(1, 16)
     random_tour.append(a)
 
+
 @app.route('/')
 def main():
-    return render_template('index.html', about_tour=tours, logo=description, random_tours = random_tour,
+    return render_template('index.html', about_tour=tours, logo=description, random_tours=random_tour,
                            lst_departures=list_departures, info_tour=search_tours, about_departures=departures)
 
 
