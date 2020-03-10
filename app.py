8fd3316ebe2535f35fc76cfb586675eb122cb059
BeceJlka <59609028+BeceJlka@users.noreply.github.com>
2020-03-10 15:39:18 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,36 +1,70 @@
-from flask import Flask, render_template
-from data import *
+from flask import Flask, render_template, request
+import json
+from data import teachers, goals, days
 import random
-
+import pprint
 app = Flask(__name__)
-
-random_tour = []
-search_tours = []
-for key, value in tours.items():
-    search_tours.append(value)
-for i in range(6):
-    a = random.randint(1, 16)
-    random_tour.append(a)
-
+with open('teachers.json', "w") as file_write:
+    json.dump(teachers, file_write)
+with open("teachers.json", "r") as f:
+    teachers = json.load(f)
 
 @app.route('/')
 def main():
-    return render_template('index.html', about_tour=tours, logo=description, random_tours=random_tour,
-                           lst_departures=list_departures, info_tour=search_tours, about_departures=departures)
+    random_teacher = []
+    for i in range(6):
+        a = random.randint(1, 11)
+        random_teacher.append(a)
+    pprint.pprint(teachers)
+    return render_template('index.html',teacher = teachers, random = random_teacher)
 
 
-@app.route('/departure/<departure>')
-def render_departure(departure):
-    return render_template('departure.html', logo=description, title=subtitle,
-                           info_deature=search_tours, about_departures=departures,
-                           about_tour=tours, info_tour=search_tours, d=departure)
+@app.route('/goals/<goal>/')
+def render_goals(goal):
+    return render_template('goal.html')
+
+@app.route('/all_profiles/')
+def render_all_profile():
+    return render_template('all_profile.html',teacher = teachers)
 
 
-@app.route('/tour/<id>')
-def tour(id):
-    return render_template('tour.html', about_tour=tours[int(id)], about_departures=departures,
-                           info_tour=search_tours)
+@app.route('/profiles/<int:id>/')
+def render_profile(id):
+    return render_template('profile.html', teacher=teachers, goals=goals, id=id, days = days)
 
 
-if __name__ == '__main__':
-    app.run(port=8000,debug=False)
+@app.route('/request/')
+def render_request():
+    return render_template('request.html', teacher=teachers)
+
+
+@app.route('/request_done/')
+def render_request_done():
+    return render_template('request_done.html')
+
+
+@app.route('/booking/<int:id>/<day>/<time>/')
+def rander_booking(id, day, time):
+    return render_template('booking.html',teacher=teachers, id=id, day = day, time = time, days = days)
+
+
+@app.route('/booking_done/', methods=['POST'])
+def rander_booking_done():
+    booking = dict.fromkeys(['cteacher', 'day', 'time', 'username', 'phone'])
+    cteacher = request.form.get("clientTeacher")
+    booking['cteacher'].append(cteacher)
+    day = request.form.get("clientWeekday")
+    booking['day'].append(day)
+    time = request.form.get("clientTime")
+    booking['time'].append(time)
+    username = request.form.get("clientName")
+    booking['username'].append(username)
+    phone = request.form.get("clientPhone")
+    booking['phone'].append(phone)
+    with open('booking.json', "w") as f_write:
+        json.dump(booking,f_write)
+    print(booking)
+    return render_template('booking_done.html', username=username, phone=phone, day=day, time=time, days=days, teacher=teachers, id=cteacher)
+
+
+app.run()
